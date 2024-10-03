b2093487c9e5a33fc3eb61e5b779d81510019f95
resilience-me <emailsareobsolete@gmail.com>
2024-10-04 01:45:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -297,7 +297,7 @@ async function handleFunctionSelect() {
             <label for="account">Account:</label>
             <input type="text" id="account" placeholder="Enter account address" class="address-input">
         `;
-    } else if (selectedFunction === 'hour' || selectedFunction === 'pseudonymEvent' || selectedFunction === 'toSeconds' || selectedFunction === 'registryLength' || selectedFunction === 'seed' ) {
+    } else if (selectedFunction === 'hour' || selectedFunction === 'pseudonymEvent' || selectedFunction === 'toSeconds' || selectedFunction === 'registryLength' || selectedFunction === 'seed' || selectedFunction === 'shuffled' || selectedFunction === 'population' || selectedFunction === 'courts' || selectedFunction === 'permits' ) {
         inputFieldsDiv.innerHTML = `
             <label for="t">Period:</label>
             <input type="text" id="t" placeholder="Enter value">
